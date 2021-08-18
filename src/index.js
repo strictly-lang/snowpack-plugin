078e210a41a5bd69db8abb29417ce6d1f0063f41
Carlo Jeske <jeske@sc-networks.com>
2021-08-18 15:04:59 +0200

    fix(path): use dirname from pluginconfig

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const strictly = require("@strictly-lang/compiler")
 
-module.exports = function (_snowpackConfig, _pluginOptions) {
-  const compiler = strictly({ cwd: __dirname });
+module.exports = function (_snowpackConfig, pluginOptions) {
+  const compiler = strictly({ cwd: pluginOptions.cwd });
 
   return {
     name: 'strictly-plugin',
